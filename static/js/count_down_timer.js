cfce2d8c141c5dd44631df73b29940a82203de8f
Zenikolas <zenikolas256@gmail.com>
2018-05-28 15:40:43 +0300

    count-down timer ticks right after initialization

--- static/js/count_down_timer.js
+++ static/js/count_down_timer.js
@@ -1,30 +1,33 @@
-const second = 1000,
+var  second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
 
 countdown = document.getElementById("count_down_timer");
+final_date = new Date('Jun 3, 2018 10:45:00').getTime()
 
-let countDown = new Date('Jun 3, 2018 10:45:00').getTime(),
-    x = setInterval(function() {
+var renderCountDown = function() {
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+    now = new Date().getTime();
+    distance = final_date - now;
 
-      days = Math.floor(distance / (day));
-      hours = Math.floor((distance % (day)) / (hour));
-      minutes = Math.floor((distance % (hour)) / (minute));
-      seconds = Math.floor((distance % (minute)) / second);
+    days = Math.floor(distance / (day));
+    hours = Math.floor((distance % (day)) / (hour));
+    minutes = Math.floor((distance % (hour)) / (minute));
+    seconds = Math.floor((distance % (minute)) / second);
 
-      if (distance < 0) {
-          countdown.innerHTML = "<h1 class=\"title general_title big_title\">Just married!</h1>"
-      } else {
-	    countdown.innerHTML = "<ul class=\"box_block general_title text\">"
+    if (distance < 0) {
+        countdown.innerHTML = "<h1 class=\"title general_title big_title\">Just married!</h1>"
+    } else {
+        countdown.innerHTML = "<ul class=\"box_block general_title text\">"
             + "<li><span>" + days + "</span>Days</li>"
             + "<li><span>" + hours + "</span>Hours</li>"
             + "<li><span>" + minutes + "</span>Minutes</li>"
             + "<li><span>" + seconds + "</span>Seconds</li>"
             + "</ul>";
-      }
+    }
+}
 
-    }, second)
+
+setInterval(renderCountDown, second);
+renderCountDown();
